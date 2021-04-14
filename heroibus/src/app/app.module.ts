a7e051462a7bcc386a92122f9c44ab2e0c0de851
Srdan Turudic <sr.dan@hotmail.com>
2021-04-14 14:26:05 +0200

    feat(logo): add logo atom

--- heroibus/src/app/app.module.ts
+++ heroibus/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/atoms/button/button.component';
+import { LogoComponent } from './components/atoms/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
